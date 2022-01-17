a12049ef7c52a7592e9cb9a91bbb1536c7db12a3
tiennt99 <tiennt180799@gmail.com>
2022-01-17 23:13:49 +0700

    manage role

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="E:\Study\Backend\Flask\FlaskApi\venv"
+$env:VIRTUAL_ENV="D:\Đồ Án\Project\FlaskApiDocs\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
